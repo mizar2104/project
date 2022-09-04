4fc424442ae62c8c1d31febac9534e02eb714e7f
mizar2104 <mizarharisto2104@gmail.com>
2022-09-04 18:08:39 +0700

    menambahkan dan mengubag jumbotron

--- js/script.js
+++ js/script.js
@@ -15,5 +15,21 @@ $('body').animate({
 }, 1000, 'easeInOutExpo');
 });
 
+// parallax
+$(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
+    $('.jumbotron img').css({
+        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+    });
+    
+    $('.jumbotron p').css({
+        'transform' : 'translate(0px, '+ wScroll/1.5 +'%)'
+    });
+});
+
 
 
